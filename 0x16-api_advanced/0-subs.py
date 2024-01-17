54f5ffd5f8415cf41ffae1d303565d6da4ed0958
willyopera <willy4opera@gmail.com>
2024-01-17 09:30:52 -0500

    Debugged the 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    HD = {
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(link, headers=HD, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
