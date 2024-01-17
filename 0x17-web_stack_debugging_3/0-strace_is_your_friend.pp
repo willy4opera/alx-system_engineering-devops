8f840a7ac4f09a6cfe54fe5966b6c4b66a9a5a49
willyopera <willy4opera@gmail.com>
2024-01-17 18:42:11 -0500

    Debugged  0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,10 @@
 # A puppet script to replace error file on server
 
-$Error_File = '/var/www/html/wp-settings.php'
+$error_file = '/var/www/html/wp-settings.php'
 
 #replace line containing "phpp" with "php"
 
-exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${Error_File}",
+exec { 'fix error file':
+  command => "sed -i 's/phpp/php/g' ${error_file}",
   path    => ['/bin','/usr/bin']
 }
